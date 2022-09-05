b445816bf0115039a705a4633edd4fac4daffb0c
Aldo Hernández <aldohernandez@utez.edu.mx>
2022-09-05 18:01:06 -0500

    get all

--- post.js
+++ post.js
@@ -1,3 +1,21 @@
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+function showContent() {
+    console.log("Llega a la función");
+    fetch('https://reqres.in/api/users?page=2')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.data);
+        let rows = "";
+        data.data.forEach(element => {
+            rows += `<tr>
+                <td>${element.id}</td>
+                <td>${element.first_name}</td>
+                <td>${element.last_name}</td>
+                <td>${element.email}</td>
+            </tr>`;
+        });
+        
+
+        $("#results").append(rows);
+    });
+
+}
