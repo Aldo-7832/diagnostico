85cb22c4ef9cfc4b6b421ff279dee567e887ba3c
Aldo Hernández <aldohernandez@utez.edu.mx>
2022-09-05 23:47:12 -0500

    Create function

--- post.js
+++ post.js
@@ -1,21 +1,42 @@
-function showContent() {
+function getAll() {
     console.log("Llega a la función");
     fetch('https://reqres.in/api/users?page=2')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.data);
-        let rows = "";
-        data.data.forEach(element => {
-            rows += `<tr>
+        .then(response => response.json())
+        .then(data => {
+            let rows = "";
+            data.data.forEach(element => {
+                rows += `<tr>
                 <td>${element.id}</td>
                 <td>${element.first_name}</td>
                 <td>${element.last_name}</td>
                 <td>${element.email}</td>
+                <td><img src='${element.avatar}'/></td>
             </tr>`;
+            });
+            $("#results").append(rows);
         });
-        
 
-        $("#results").append(rows);
-    });
+}
 
+function create() {
+    let user = {
+        "name": document.getElementById("name").value,
+        "job": document.getElementById("job").value
+    }
+    fetch('https://reqres.in/api/users', {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    }).then(response => response.json())
+        .then(json => {
+            console.log(json);
+            let rows = "";
+            rows += `<tr>
+                <td>${json.id}</td>
+                <td>${json.name}</td>
+                <td>${json.job}</td>
+                </tr>`;
+            $("#resultsCreate").append(rows);
+        })
+        .catch(err => console.log(err));
 }
